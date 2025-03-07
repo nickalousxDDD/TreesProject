9152fe84cffbaf2eec35eb0767d44d69177067cf
Nicholas Hoang <149106894+nickalousxDDD@users.noreply.github.com>
2025-03-07 14:32:18 -0800

    Added classes to structure bst

--- TreesProject.java
+++ TreesProject.java
@@ -1,6 +1,26 @@
+import java.util.Stack;
+
 public class TreesProject {
 
     public static void main(String[] args) {
         System.out.println("hello wrld");
     }
+
+public static class BST {
+
+    //represents each node in the tree
+public static class Node {
+    int key; 
+    Node left, right;
+
+public Node(int item) {
+    key = item;
+    left = right = null;
+}
+        }    
+    //root of the bst   
+Node root; 
+
+                }
+
 }
